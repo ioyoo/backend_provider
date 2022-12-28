8b79b02d1800ab82b62ef6db694b578b7246dfdd
Andrés Naves <andres.naves.mauri@hp.com>
2022-12-28 21:19:18 +0100

    basic structure for api calls created

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask
+from flask_restful import Api, Resource
+
 app = Flask(__name__)
+api = Api(app)
+
+users = [{"name": "me"}, {"name": "mantas", "counter": 3123513471}]
+id = 0
 
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+class UserAPI(Resource):
+    def get(self, name):
+        for user in users:
+            if user["name"] == name:
+                return user
+
+    def post(self, name):
+        global id
+        user = {"name": name, "id": id}
+        id += 1
+        users.append(user)
+        return user
+
+
+api.add_resource(UserAPI, "/users/<string:name>", endpoint="user")

d32db19e48a258798b5ba32c810b0145d3ca96e0
andres-naves-mauri <andres.naves.mauri@hp.com>
2023-01-27 20:54:22 +0100

    Get returns list of daily stocks

--- lambdas/daily/lambda_function.py
+++ lambdas/daily/lambda_function.py
@@ -6,19 +6,20 @@ from  data.credentials import DBCredentials
 def lambda_handler(event, context):
     method = event["httpMethod"]
     if(method == "GET"):
-        pass
+        awsdb = AWSDatabase()
+        print(awsdb.queryAllData())
     elif(method == "POST"):
         pass
         
     
 class AWSDatabase:
     
-    def __init__(self, table) -> None:
+    def __init__(self) -> None:
         self.cred = DBCredentials()
         self.engine = db.create_engine(f'mysql+pymysql://{self.cred.USER}:{self.cred.PASS}@{self.cred.URL}:{self.cred.PORT}/{self.cred.DB_NAME}')
         self.conn = self.engine.connect()
         self.metadata = db.MetaData()
-        self.division= db.Table(table, self.metadata, autoload=True, autoload_with=self.engine)
+        self.division= db.Table("Daily", self.metadata, autoload=True, autoload_with=self.engine)
     
     def queryAllData(self):
         """ returns all data in database
@@ -29,7 +30,4 @@ class AWSDatabase:
         query =  self.division.select()
         exe = self.conn.execute(query)
         return exe.fetchall()
-    
-
-lambda_handler(1,1)
 
--- upload.bash
+++ upload.bash
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 export LAMBDA=$1
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+if [ -z "$LAMBDA" ]; then
+    echo -e "${RED} Error: No lambda given to upload, add it as first argument in upload call!"
+    exit 1
+fi
 
 deactivate
 
@@ -12,12 +19,17 @@ echo "Adding new lambda function and local dependencies to deployment_package...
 cd ../../../../
 zip -r deployment_package.zip data
 zip -r deployment_package.zip private
-zip -g deployment_package.zip lambdas/$LAMBDA/lambda_function.py
+zip -g -j deployment_package.zip ./lambdas/$LAMBDA/lambda_function.py
 mv deployment_package.zip lambdas/$LAMBDA 
 
 echo "Publishing deployment_package to aws!"
 aws lambda update-function-code --function-name $LAMBDA --zip-file fileb://lambdas/$LAMBDA/deployment_package.zip
 
-echo "Deployment Complete!!!"
+retval=$?
+if [ $retval -ne 0 ]; then
+    echo -e "${RED} Error: could not complete upload..."
+    exit 1
+fi
+echo -e "${GREEN} Deployment Complete!!!"
 
 source .env/bin/activate
